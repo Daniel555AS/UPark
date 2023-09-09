5681faf60e8a65c07626c2eb76e2fb08f75b68a0
Daniel555AS <daniel57orlando@gmail.com>
2023-09-09 17:56:43 -0500

    Login

--- src/co/edu/upb/upark/DatabaseConnection.java
+++ src/co/edu/upb/upark/DatabaseConnection.java
@@ -11,7 +11,7 @@ public class DatabaseConnection {
     private static final String PASSWORD = "***";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(URL, USER, PASSWORD); 
     }
 
 }
