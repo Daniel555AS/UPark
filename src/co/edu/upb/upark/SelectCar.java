0bea77ced23d5668d9677439c96333249df74764
Daniel555AS <daniel57orlando@gmail.com>
2023-09-10 17:01:50 -0500

    Login updates.

--- src/co/edu/upb/upark/SelectCar.java
+++ src/co/edu/upb/upark/SelectCar.java
@@ -24,6 +24,8 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import javax.swing.Timer;
+
 public class SelectCar extends JFrame {
 
 	private JPanel contentPane;
@@ -48,13 +50,13 @@ public class SelectCar extends JFrame {
 	 * Create the frame.
 	 */
 	public SelectCar() {
-		
-		
-		
+
+
+
 		int numberPosition = Login.positionNumber;
 		String numberIdentification = Login.IdentificationNumber;
-		
-		
+
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//setBounds(100, 100, 649, 390);
 		setBounds(100, 100, 1286, 660);
@@ -64,29 +66,29 @@ public class SelectCar extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 594, 1287, 29);
 		panel.setBackground(new Color(0, 0, 0));
 		contentPane.add(panel);
-		
+
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setBounds(10, 10, 343, 132);
 		contentPane.add(lblNewLabel);
 		lblNewLabel.setIcon(new ImageIcon("Media\\logo-upb-blanco1.png"));
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Escoge tu vehículo");
 		lblNewLabel_1.setFont(new Font("HP Simplified", Font.BOLD, 99));
 		lblNewLabel_1.setBounds(238, 124, 795, 124);
 		contentPane.add(lblNewLabel_1);
-		
+
 		String name = "";
 		String[] vehicles = new String[2];
-	
 
-		
+
+
 		// --------------------- Get the name of the vehicle owner  and the array of vehicles ---------------------
-		
+
 		try {
 			Connection conn = DriverManager.getConnection("jdbc:mysql://35.222.147.13:3306/parqueadero", "root", "842963");
 			Statement stmt1 = conn.createStatement();
@@ -99,76 +101,103 @@ public class SelectCar extends JFrame {
 			String[] arraySelectCar1 = listSelectCar1.toArray(new String[0]);
 			rs.close();
 			stmt1.close();
-			
+
 			name = arraySelectCar1[numberPosition];			
-			
+
 			Statement stmt3 = conn.createStatement();
-			
+
 			String query = "SELECT * FROM vehiculos WHERE numeroidentificacion = '" + numberIdentification + "'"; 
 			ResultSet rs3 = stmt3.executeQuery(query);
 			String rowString1 = "";
 			String rowString2 = "";
 			int count = 0;
 			while (rs3.next()) {
-			    count++;
-			    if (count == 1) {
-			        for (int jj = 2; jj <= rs3.getMetaData().getColumnCount(); jj++) {
-			            rowString1 += rs3.getString(jj) + "  ";
-			        }
-			    } else if (count == 2) {
-			        for (int jj = 2; jj <= rs3.getMetaData().getColumnCount(); jj++) {
-			            rowString2 += rs3.getString(jj) + "  ";
-			        }
-			    }
+				count++;
+				if (count == 1) {
+					for (int jj = 2; jj < rs3.getMetaData().getColumnCount(); jj++) {
+						rowString1 += rs3.getString(jj) + "  ";
+					}
+				} else if (count == 2) {
+					for (int jj = 2; jj < rs3.getMetaData().getColumnCount(); jj++) {
+						rowString2 += rs3.getString(jj) + "  ";
+					}
+				}
 			}
-			
+
 			vehicles[0] = rowString1;
 			vehicles[1] = rowString2;
-			
+
 			rs3.close();
 			stmt3.close();
-			
-			
+
+
 			conn.close();
 		}
 		catch(SQLException i){
 			i.printStackTrace();
 		}
 
-		
+
 		// ------------------------------------------------------------------------------
-		
-		
-		
-		
+
+
+
+
 		JLabel lblNewLabel_2 = new JLabel(name);
 		lblNewLabel_2.setFont(new Font("HP Simplified", Font.BOLD, 36));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setBounds(192, 258, 888, 49);
 		contentPane.add(lblNewLabel_2);
-		
+
 		JComboBox comboBox = new JComboBox();
 		DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>();
-		model.addElement(vehicles[0]);
-		model.addElement(vehicles[1]);
+		if(vehicles[0] != "") {
+			model.addElement(vehicles[0]);
+		}
+		if(vehicles[1] != "") {
+			model.addElement(vehicles[1]);
+		}
+
 		comboBox.setModel(model);
 		comboBox.setFont(new Font("HP Simplified", Font.BOLD, 29));
 		comboBox.setBounds(216, 345, 840, 71);
 		contentPane.add(comboBox);
+		
 		
 		JButton btnNewButton = new JButton("INGRESAR");
 		btnNewButton.setFont(new Font("HP Simplified", Font.BOLD, 31));
 		btnNewButton.setForeground(new Color(0, 0, 0));
 		btnNewButton.setBackground(new Color(255, 239, 91));
 		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
+		    public void actionPerformed(ActionEvent e) {
+		        // close SelectCar
+		        dispose();
+
+		        // display Success
+		        Success s = new Success();
+		        s.setVisible(true);
+
+		        // create a Swing Timer with a 5-second delay
+		        Timer timer = new Timer(5000, new ActionListener() {
+		            public void actionPerformed(ActionEvent e) {
+		                // close JFrame2
+		                s.dispose();
+
+		                // display Login
+		                Login l = new Login();
+		                l.setVisible(true);
+		            }
+		        });
+
+		        timer.setRepeats(false);
+		        timer.start(); // start the timer
+		    }
 		});
 		btnNewButton.setBounds(416, 468, 440, 76);
 		contentPane.add(btnNewButton);
-		
-		
-		
-		
+
+
+
+
 	}
 }
---
+++ src/co/edu/upb/upark/Success.java
@@ -0,0 +1,112 @@
+package co.edu.upb.upark;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class Success extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Success frame = new Success();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Success() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1286, 660);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 594, 1287, 29);
+		panel.setBackground(new Color(0, 0, 0));	
+		contentPane.add(panel);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(10, 10, 343, 132);
+		lblNewLabel.setIcon(new ImageIcon("Media\\logo-upb-blanco1.png"));
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("INGRESO EXITOSO");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("HP Simplified", Font.BOLD, 99));
+		lblNewLabel_1.setBounds(188, 153, 895, 124);
+		contentPane.add(lblNewLabel_1);
+		
+
+		
+		String name = "";
+		int numberPosition = Login.positionNumber;
+		
+		// ------------------ Get the name of the vehicle owner ------------------
+		
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://35.222.147.13:3306/parqueadero", "root", "842963");
+			Statement stmtSuccess = conn.createStatement();
+			ResultSet rsSuccess = stmtSuccess.executeQuery("SELECT nombre FROM usuarios");
+			ArrayList<String> listSuccess = new ArrayList<>();
+			while (rsSuccess.next()) {
+				String valor = rsSuccess.getString("nombre");
+				listSuccess.add(valor);
+			}
+			String[] arraySuccess= listSuccess.toArray(new String[0]);
+			rsSuccess.close();
+			stmtSuccess.close();
+
+			name = arraySuccess[numberPosition];	
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// -----------------------------------------------------------------------
+		
+		JLabel lblNewLabel_2 = new JLabel(name);
+		lblNewLabel_2.setFont(new Font("HP Simplified", Font.BOLD, 36));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setBounds(195, 301, 888, 49);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("¡Gracias Por Usar Nuestro Servicio!");
+		lblNewLabel_3.setForeground(new Color(172, 50, 9));
+		lblNewLabel_3.setFont(new Font("HP Simplified", Font.BOLD, 70));
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setBounds(97, 410, 1078, 82);
+		contentPane.add(lblNewLabel_3);
+		
+	}// public Success()
+
+}
