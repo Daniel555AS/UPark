6753fc9ad4cf89f4893bdaf074b2674ac2a56492
rafer <rafer@Rloz>
2023-09-24 18:46:07 -0500

    Salida

---
+++ src/co/edu/upb/upark/DatabaseManager.java
@@ -0,0 +1,97 @@
+package co.edu.upb.upark;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DatabaseManager {
+
+	private Connection conn;
+
+	public DatabaseManager() {
+
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://:/", "***", "***");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	} // public DatabaseManager()
+	
+	
+    public String[] getIdNumberDataFromUsuarios() {
+    	
+        String[] idNumberDataFromUsuarios = null;
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT numeroidentificacion FROM usuarios");
+
+            ArrayList<String> listIdNumberDataFromUsuarios = new ArrayList<>();
+            while (rs.next()) {
+                String valor = rs.getString("numeroidentificacion");
+                listIdNumberDataFromUsuarios.add(valor);
+            }
+            idNumberDataFromUsuarios = listIdNumberDataFromUsuarios.toArray(new String[0]);
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return idNumberDataFromUsuarios;
+        
+    } //  public String[] getIdNumberDataFromUsuarios()
+    
+    
+    public String[] getRolDataFromUsuarios() {
+        String[] rolDataFromUsuarios = null;
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT rol FROM usuarios");
+            ArrayList<String> listRolDataFromUsuarios = new ArrayList<>();
+            while (rs.next()) {
+                String valor = rs.getString("rol");
+                listRolDataFromUsuarios.add(valor);
+            }
+            rolDataFromUsuarios = listRolDataFromUsuarios.toArray(new String[0]);
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rolDataFromUsuarios;
+    } //
+    
+    
+    public String[] getCurrentUserDataFromUsuariosActuales() {
+        String[] currentUsersDataFromUsuariosActuales = null;
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT NumeroIdentificacion FROM usuariosActuales");
+            ArrayList<String> listCurrentUsersFromUsuariosActuales = new ArrayList<>();
+            while (rs.next()) {
+                String valor = rs.getString("NumeroIdentificacion");
+                listCurrentUsersFromUsuariosActuales.add(valor);
+            }
+            currentUsersDataFromUsuariosActuales = listCurrentUsersFromUsuariosActuales.toArray(new String[0]);
+            stmt.close();
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return currentUsersDataFromUsuariosActuales;
+    } // public String[] getCurrentUserDataFromUsuariosActuales()
+    
+    
+    public void closeConnection() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } // public void closeConnection()
+    
+	
+} // public class DatabaseManager
--- src/co/edu/upb/upark/Exit.java
+++ src/co/edu/upb/upark/Exit.java
@@ -11,131 +11,195 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Exit extends JFrame {
 
-    private static final long serialVersionUID = 1L;
-    private JPanel contentPane;
-    private static String nameForExit = "";
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private static String nameForExit = "";
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            try {
-                Exit frame = new Exit();
-                frame.setVisible(true);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-    }
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			try {
+				Exit frame = new Exit();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
 
-    public Exit() {
-        this.setResizable(false); // Desabilitar maximizar /Razon ... Dimensionar
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 1286, 660);
-        contentPane = new JPanel();
-        contentPane.setBackground(new Color(255, 255, 255));
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
+	public Exit() {
+		
+		this.setResizable(false); // Disable the maximize window option
+		
+		String numberIdentification = Login.IdentificationNumber;
+		String vehiclePlate = "";
+		String vehicleInformation = "";
 
-        JPanel panel = new JPanel();
-        panel.setBackground(new Color(0, 0, 0));
-        panel.setBounds(0, 594, 1287, 29);
-        contentPane.add(panel);
+		// -------------------------------------------------- Get the Vehicle Information  --------------------------------------------------
 
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://:/", "***", "***")) {
-            try (Statement statement = connection.createStatement()) {
-                try (ResultSet resultSet = statement.executeQuery("SELECT NombreUsuario FROM usuariosActuales WHERE NumeroIdentificacion = '" + Login.IdentificationNumberExit + "'")) {
-                    if (resultSet.next()) {
-                        nameForExit = resultSet.getString("NombreUsuario");
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://:/", "***", "***");
 
-        JLabel lblNewLabel = new JLabel("CONFIRMACION DE DATOS PARA SALIDA");
-        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        lblNewLabel.setFont(new Font("Cambria", Font.BOLD, 40));
-        lblNewLabel.setBounds(188, 152, 895, 72);
-        contentPane.add(lblNewLabel);
+			Statement stmt1 = conn.createStatement();
+			String sql1 = "SELECT * FROM usuariosActuales WHERE numeroidentificacion = '" + numberIdentification + "'";
+			ResultSet rs1 = stmt1.executeQuery(sql1);
 
-        JLabel lblNewLabel_1 = new JLabel("");
-        lblNewLabel_1.setBounds(10, 10, 343, 132);
-        lblNewLabel_1.setIcon(new ImageIcon("Media\\logo-upb-blanco1.png"));
-        contentPane.add(lblNewLabel_1);
+			while (rs1.next()) {          
+				vehiclePlate = rs1.getString("placa");
+			}
+			rs1.close();
+			stmt1.close();
 
-        //Boton para Confirmar (El color de fondo funciona solo al ejecutar)/Sin razon ..?
-        JButton confirmarButton = new JButton("CONFIRMAR");
-        confirmarButton.setBackground(new Color(255, 239, 91));
-        confirmarButton.setForeground(new Color(172, 50, 9));
-        confirmarButton.setFont(new Font("Cambria", Font.BOLD, 50));
-        confirmarButton.setBounds(826, 454, 335, 82);
-        contentPane.add(confirmarButton);
-        
+			Statement stmt2 = conn.createStatement();
+			String sql2 = "SELECT * FROM vehiculos WHERE placa = '" + vehiclePlate + "'";
+			ResultSet rs2 = stmt2.executeQuery(sql2);
 
-        confirmarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try (Connection connection = DriverManager.getConnection("jdbc:mysql://:/", "***", "***")) {
-                    try (Statement statement = connection.createStatement()) {
-                        String deleteQuery = "DELETE FROM usuariosActuales WHERE NumeroIdentificacion = '" + Login.IdentificationNumberExit + "'";
-                        statement.executeUpdate(deleteQuery);
-                    }
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
+			while (rs2.next()) {          
+				String vehicleBrand = rs2.getString("marca");
+				String vehicleColor = rs2.getString("color");
+				String vehicleModel = rs2.getString("modelo");
 
-                JOptionPane optionPane = new JOptionPane("Salida confirmada. Gracias por usar nuestros servicios", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new Object[]{}, null);
+				vehicleInformation = vehicleBrand + " " + vehiclePlate + " " + vehicleColor + " " + vehicleModel;
+			}
+			rs2.close();
+			stmt2.close();
+			conn.close();
 
-                // Crear un JDialog sin botones
-                JDialog dialog = new JDialog(Exit.this, "Confirmación", true);
-                dialog.setContentPane(optionPane);
+		}
+		catch(SQLException i){
+			i.printStackTrace();
+		}
 
-                // Establecer un temporizador para cerrar el diálogo después de 2 segundos
-                Timer timer = new Timer(2000, new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent evt) {
-                        dialog.dispose();
-                        // Volver a la ventana de login
-                        Login loginFrame = new Login();
-                        loginFrame.setVisible(true);
-                        dispose();
-                    }
-                });
+		// ----------------------------------------------------------------------------------------------------------------------------------
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1286, 660);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		// Creation a JPanel for decorative purposes(The JPanel located at the bottom):
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0, 0, 0));
+		panel.setBounds(0, 594, 1287, 29);
+		contentPane.add(panel);
 
-                timer.setRepeats(false);
-                timer.start();
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://:/", "***", "***")) {
+			try (Statement statement = connection.createStatement()) {
+				try (ResultSet resultSet = statement.executeQuery("SELECT NombreUsuario FROM usuariosActuales WHERE NumeroIdentificacion = '" + Login.IdentificationNumberExit + "'")) {
+					if (resultSet.next()) {
+						nameForExit = resultSet.getString("NombreUsuario");
+					}
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-                dialog.pack();
-                dialog.setLocationRelativeTo(Exit.this);
-                dialog.setVisible(true);
-            }
-        });
+		// Creation of a JLabel containing the text: "Confirmación De Datos Para Salida":
+		JLabel lblConfirmationOfData = new JLabel("Confirmación De Datos Para Salida");
+		lblConfirmationOfData.setHorizontalAlignment(SwingConstants.CENTER);
+		lblConfirmationOfData.setFont(new Font("Cambria", Font.BOLD, 66));
+		lblConfirmationOfData.setBounds(63, 152, 1098, 88);
+		contentPane.add(lblConfirmationOfData);
 
-        JLabel lblNewLabel_2 = new JLabel(nameForExit);
-        lblNewLabel_2.setFont(new Font("Cambria", Font.BOLD, 36));
-        lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-        lblNewLabel_2.setBounds(188, 257, 888, 49);
-        contentPane.add(lblNewLabel_2);
+		// Creation of a JLabel with the Logo of the University:
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setBounds(10, 10, 343, 132);
+		lblNewLabel_1.setIcon(new ImageIcon("Media\\logo-upb-blanco1.png"));
+		contentPane.add(lblNewLabel_1);
 
-        //Boton para regresar (El color de fondo funciona solo al ejecutar)/Sin razon ..?
-        JButton regresarButton = new JButton("REGRESAR");
-        regresarButton.setBackground(new Color(255, 239, 91));
-        regresarButton.setHorizontalAlignment(SwingConstants.CENTER);
-        regresarButton.setForeground(new Color(172, 50, 9));
-        regresarButton.setFont(new Font("Cambria", Font.BOLD, 50));
-        regresarButton.setBounds(64, 454, 385, 82);
-        contentPane.add(regresarButton);
+		// Confirmation Button Properties:
+		JButton confirmButton = new JButton("CONFIRMAR");
+		confirmButton.setBackground(new Color(255, 239, 91));
+		confirmButton.setForeground(new Color(0, 0, 0));
+		confirmButton.setFont(new Font("Cambria", Font.BOLD, 50));
+		confirmButton.setBounds(826, 454, 335, 82);
+		contentPane.add(confirmButton);
 
-        regresarButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                Login loginFrame = new Login();
-                loginFrame.setVisible(true);  
-                dispose();  
-            }
-        });
-    }
+		confirmButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try (Connection connection = DriverManager.getConnection("jdbc:mysql://:/", "***", "***")) {
+					try (Statement statement = connection.createStatement()) {
+						String deleteQuery = "DELETE FROM usuariosActuales WHERE NumeroIdentificacion = '" + Login.IdentificationNumberExit + "'";
+						statement.executeUpdate(deleteQuery);
+					}
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+
+				JOptionPane optionPane = new JOptionPane("Salida confirmada. Gracias por usar nuestros servicios", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new Object[]{}, null);
+
+				// Crear un JDialog sin botones
+				JDialog dialog = new JDialog(Exit.this, "Confirmación", true);
+				dialog.setContentPane(optionPane);
+
+				// Establecer un temporizador para cerrar el diálogo después de 2 segundos
+				Timer timer = new Timer(2000, new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent evt) {
+						dialog.dispose();
+						// Volver a la ventana de login
+						Login loginFrame = new Login();
+						loginFrame.setVisible(true);
+						dispose();
+					}
+				});
+
+				timer.setRepeats(false);
+				timer.start();
+
+				dialog.pack();
+				dialog.setLocationRelativeTo(Exit.this);
+				dialog.setVisible(true);
+			}
+		});
+
+		// Creation of a JLabel containing the User's Name:
+		JLabel lblNameExit = new JLabel(nameForExit);
+		lblNameExit.setForeground(new Color(0, 0, 0));
+		lblNameExit.setFont(new Font("Cambria", Font.BOLD, 45));
+		lblNameExit.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNameExit.setBounds(63, 284, 1098, 49);
+		contentPane.add(lblNameExit);
+
+		//Boton para regresar (El color de fondo funciona solo al ejecutar)/Sin razon ..?
+		JButton regresarButton = new JButton("REGRESAR");
+		regresarButton.setBackground(new Color(255, 239, 91));
+		regresarButton.setHorizontalAlignment(SwingConstants.CENTER);
+		regresarButton.setForeground(new Color(0, 0, 0));
+		regresarButton.setFont(new Font("Cambria", Font.BOLD, 50));
+		regresarButton.setBounds(64, 454, 385, 82);
+		contentPane.add(regresarButton);
+		
+		// Creation of a JLabel containing the vehicle Information:
+		JLabel lblVehicleInformation = new JLabel(vehicleInformation);
+		lblVehicleInformation.setForeground(new Color(75, 75, 75));
+		lblVehicleInformation.setHorizontalAlignment(SwingConstants.CENTER);
+		lblVehicleInformation.setFont(new Font("Cambria", Font.BOLD, 35));
+		lblVehicleInformation.setBounds(74, 371, 1088, 59);
+		contentPane.add(lblVehicleInformation);
+		
+		// Creation of a JLabel containing the text: "Vehículo:":
+		JLabel lblNewLabel_3 = new JLabel("Vehículo:");
+		lblNewLabel_3.setForeground(new Color(156, 18, 18));
+		lblNewLabel_3.setFont(new Font("Cambria", Font.BOLD, 35));
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setBounds(74, 343, 1088, 32);
+		contentPane.add(lblNewLabel_3);
+
+		regresarButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Login loginFrame = new Login();
+				loginFrame.setVisible(true);  
+				dispose();  
+			}
+		});
+	}
 }
